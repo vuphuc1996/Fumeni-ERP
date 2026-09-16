747c8fd5f41995a0cd5be85c3d31b3702e4f42ca
vuphuc1996 <34017759+vuphuc1996@users.noreply.github.com>
2026-09-16 12:02:23 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-const CACHE_NAME = "fumeni-shell-v3-13";
-const APP_SHELL = ["./", "./index.html", "./manifest.json", "./logo.png", "./sw.js"];
-self.addEventListener("install", event => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
-self.addEventListener("activate", event => {
-    event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
-self.addEventListener("fetch", event => {
-    if (event.request.method !== "GET") return;
-    const u = new URL(event.request.url);
-    if (u.origin !== self.location.origin) return;
-    event.respondWith(fetch(event.request).then(response => {
-        if (response.ok) {
-            const copy = response.clone();
-            caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        }
-        return response;
-    }).catch(() => caches.match(event.request)));
-});
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
