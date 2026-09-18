381f081cb29ba91c04718cfd6fae863c6a892293
vuphuc1996 <34017759+vuphuc1996@users.noreply.github.com>
2026-09-18 10:44:02 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'fumenierp-cache-v2';
+const urlsToCache = [
+  './',
+  './www/index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
