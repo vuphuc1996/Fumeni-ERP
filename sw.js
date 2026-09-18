20ec5c7aec3a6229d38e09df7b8404e05ce04009
vuphuc1996 <34017759+vuphuc1996@users.noreply.github.com>
2026-09-18 12:22:35 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,14 @@
-const CACHE_NAME = 'fumenierp-cache-v2';
-const urlsToCache = [
-  './',
-  './www/index.html',
-  './manifest.json'
-];
-
+const CACHE_NAME = 'fumeni-app-shell-v4.0.2';
+const SHELL = ['./', './index.html', './manifest.json', './logo.png'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
 });
-
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
