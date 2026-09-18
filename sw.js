8c08ce5f2e614879873474e37213e6210a8b21d9
vuphuc1996 <34017759+vuphuc1996@users.noreply.github.com>
2026-09-18 10:26:17 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE = 'fumeni-shell-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
-});
