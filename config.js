fb18ba537d22d174ce19fc2af80df5ff9a75aa94
vuphuc1996 <34017759+vuphuc1996@users.noreply.github.com>
2026-09-25 12:24:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.FUMENI_RUNTIME = Object.freeze({
   // Replace after deploying the new Apps Script project.
-  GAS_URL: 'https://script.google.com/macros/s/REPLACE_WITH_NEW_DEPLOYMENT_ID/exec',
+  GAS_URL: 'https://script.google.com/macros/s/1xIGFn1HLSnJ2ONf-SgvlB4Z7DNFvVnIB9_j2mF-IhFB8X4skWpwvJ-Ka/exec',
   LOGO_URL: './logo.png',
   BACKGROUND_URL: './background.svg'
 });
